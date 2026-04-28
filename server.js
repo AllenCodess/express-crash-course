0d085c1381db72cdab57c42ff52663aa53d53f8b
AllenCodess <Aph296@protonmail.com>
2026-04-28 13:53:44 -0400

    added limits to search

--- server.js
+++ server.js
@@ -12,19 +12,17 @@ let posts = [
 
 // get all posts
 (app.get("/api/posts", (req, res) => {
-  res.json(posts);
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
 }),
   //get a single posts
   (app.get("/api/posts/:id", (req, res) => {
     const id = parseInt(req.params.id);
     res.json(posts.filter((post) => post.id === id));
   }),
-  // app.get("/", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "public", "index.html"));
-  // });
-
-  // app.get("/about", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "public", "about.html"));
-  // });
-
   app.listen(port, () => console.log(`Server is running on port ${port}`))));
