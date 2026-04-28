861023897d2f5e1bb4ee3ce72969331563c31589
AllenCodess <Aph296@protonmail.com>
2026-04-28 14:06:56 -0400

    status codes for valid posts and nonvalid post responses

--- server.js
+++ server.js
@@ -15,14 +15,20 @@ let posts = [
   const limit = parseInt(req.query.limit);
 
   if (!isNaN(limit) && limit > 0) {
-    res.json(posts.slice(0, limit));
+    res.status(200).json(posts.slice(0, limit));
   } else {
-    res.json(posts);
+    res.status(200).json(posts);
   }
 }),
   //get a single posts
   (app.get("/api/posts/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    res.json(posts.filter((post) => post.id === id));
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+      res.status(404).json({ msg: `Post Not Found` });
+    } else {
+      res.status(200).json(post);
+    }
   }),
   app.listen(port, () => console.log(`Server is running on port ${port}`))));
