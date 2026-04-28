faab831427f2ef52f3c745d41f7184d33c721063
AllenCodess <Aph296@protonmail.com>
2026-04-28 14:26:42 -0400

    route files

--- server.js
+++ server.js
@@ -1,32 +1,11 @@
 const express = require("express");
 const { request } = require("http");
 const path = require("path");
+const posts = require("./routes/posts");
 const port = process.env.PORT;
 const app = express();
 
-let posts = [
-  { id: 1, title: "Post One" },
-  { id: 2, title: "Post Two" },
-  { id: 3, title: "Post Three" },
-];
+//Routes
+app.use("/api/posts", posts);
 
-// get all posts
-(app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-  res.status(200).json(posts);
-}),
-  //get a single posts
-  (app.get("/api/posts/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-
-    if (!post) {
-      return res.status(404).json({ msg: `Post Not Found` });
-    }
-    res.status(200).json(post);
-  }),
-  app.listen(port, () => console.log(`Server is running on port ${port}`))));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
